f8f7673f7ac82a3046bac923a6a0017c897e135c
Oracy Martos <oramartos_21@hotmail.com>
2019-10-28 09:09:33 -0300

    First commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'WkwV6S0PoEQqZ1PWug81erAEJ',
+  consumer_secret: 'Wj9H2ZcqtR1enUddibamGOiPbFbHdBBIC5kVzkf1YvIyLeDzdg',
+  access_token_key: '199032609-qtA8j0ly9NBgeAXzGsUxyKD0GxO0Aj66F5tmFQew',
+  access_token_secret: 'tZHPtXmzZqWgplyB3eS6fd5CDeHUIv3zLezHPaOSaBhEZ'
+}
